51e9cce38bed2b2256b50b7a15ea3b847461070a
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2023-08-28 15:55:42 +0100

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const TIME_KEY = 'videoplayer-current-time';
+const player = new Player(iframeEl);
+const currentTime = localStorage.getItem(TIME_KEY) || 0;
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+setTimeToPlayer(currentTime);
+
+function getCurrentTime(data) {
+  localStorage.setItem(TIME_KEY, data.seconds);
+}
+
+function setTimeToPlayer(time) {
+  player.setCurrentTime(time);
+}
